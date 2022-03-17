cfcf80ecf83e5ef2aba6c534a20e6e5c4e7a86fd
DESKTOP-BD3MRJN\qianc <qianchuankouya@gmail.com>
2022-03-17 13:21:08 +0900

    file整理とtodoappの編集

---
+++ pages/todoapp/todo.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app>
+        <v-container fluid>
+            <v-row>
+                <v-app-bar>
+                  <h3>ToDo list.</h3>
+                </v-app-bar>
+            </v-row>
+           
+            <v-row>
+                    <v-col>
+                        <v-sheet color="white"
+                                elevation="6"
+                                height="1161"
+                                width="1137">
+                            <v-col id="app">
+                                {{message}}
+                            </v-col>
+                            <v-footer>
+                                kouya
+                            </v-footer>
+                        </v-sheet>
+                    </v-col>
+            </v-row>
+            
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+let app =new Vue({
+    el :'#app',
+    data:{
+            message: 'kouya'
+    }
+})
+
+</script>
+
+<style>
+
+
+</style>
